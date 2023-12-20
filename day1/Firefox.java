926759dfd2e2c17590b4f757d3cf3a8ff85df359
plpriya97 <154421529+plpriya97@users.noreply.github.com>
2023-12-20 18:14:24 +0530

    Add files via upload
    day assignment

---
+++ day1/Firefox.java
@@ -0,0 +1,17 @@
+package week1.day1;
+
+public class Firefox {
+	public static void main(String[] args) {
+	int releaseyear=1998;
+	float browservertion=1002.2f;
+	String browsername="Firefox";
+	boolean isvisible=true;
+	char browserlogo='f';
+			
+				System.out.println("releaseyear:"+releaseyear);
+				System.out.println("BROWERVERTION:"+browservertion);
+				System.out.println("broswername:"+browsername);
+				System.out.println("Isvisible:"+isvisible);
+				System.out.println("browserlogo"+browserlogo);
+			}
+}
---
+++ day1/hellotestleft.java
@@ -0,0 +1,12 @@
+package week1.day1;
+
+public class hellotestleft
+{
+   public static void main(String[] args) 
+	
+   {
+		   
+		System.out.println("welcometotestleft");     
+   }
+
+}
